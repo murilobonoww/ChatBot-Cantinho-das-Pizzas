36b67df0a14170375310a0a250688016d170502f
murilobonoww <murilobonow07@gmail.com>
2025-12-16 14:13:06 -0300

    feat: add socket io to send notification alert to frontend

--- back/notificationRoutes.js
+++ back/notificationRoutes.js
@@ -10,15 +10,23 @@ router.post("/post", async (req, res) => {
     try {
         const { numero, mensagem, tipo } = req.body;
         const id = randomUUID();
-        let values = [id, numero, mensagem, tipo, 'pendente'];
+        const status_default = 'pendente'; //status default
+        let values = [id, numero, mensagem, tipo, status_default];
 
         let sql = 'INSERT INTO notificacoes (id_notificacao, numero_cliente, mensagem, tipo, status) VALUES (?, ?, ?, ?, ?)';
         const [rows] = await db.execute(sql, values)
+
+        if (global.io) {
+            global.io.emit('nova_notificacao', { id, numero, mensagem, tipo, status_default })
+        }
+        else {
+            console.warn("⚠️ Socket.IO não inicializado");
+        }
         return res.status(200).json({ message: "Notificação salva com sucesso" })
 
     } catch (error) {
         console.error("Erro ao salvar notificação:", error);
-        return res.status(200).json({ message: "Erro ao salvar a notificação"}) //Retorna 200 para não interromper fluxo do N8N
+        return res.status(200).json({ message: "Erro ao salvar a notificação" }) //Retorna 200 para não interromper fluxo do N8N
     }
 })
 
@@ -29,20 +37,20 @@ router.get("/pendentes", async (req, res) => {
         return res.status(200).json(rows);
     } catch (error) {
         console.error("Erro ao salvar notificação:", error);
-        return res.status(200).json({ message: "Erro ao listar notificações"}) //Retorna 200 para não interromper fluxo do N8N
+        return res.status(200).json({ message: "Erro ao listar notificações" }) //Retorna 200 para não interromper fluxo do N8N
     }
 })
 
 router.put("/atualizar", async (req, res) => {
     try {
-        const {id, status} = req.body;
+        const { id, status } = req.body;
         const values = [status, id]
         const sql = 'UPDATE notificacoes SET status = ? where id_notificacao = ?';
         const [rows] = await db.query(sql, values);
         return res.status(200).json({ message: "Notificação atualizada com sucesso" })
     } catch (error) {
         console.error("Erro ao atualizar notificação:", error);
-        return res.id_(200).json({ message: "Erro ao atualizar notificações"}) //Retorna 200 para não interromper fluxo do N8N
+        return res.id_(200).json({ message: "Erro ao atualizar notificações" }) //Retorna 200 para não interromper fluxo do N8N
     }
 })
 
