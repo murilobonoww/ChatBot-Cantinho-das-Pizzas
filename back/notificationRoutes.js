839876495eb0493f26779b1f0db949b60ed8c458
murilobonoww <murilobonow07@gmail.com>
2025-12-16 11:47:14 -0300

    feat: melhora na função notification/post

--- back/notificationRoutes.js
+++ back/notificationRoutes.js
@@ -5,9 +5,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 const axios = require("axios");
 
+router.post("/post", async (req, res) => {
+    try {
+        const { numero, mensagem, tipo } = req.body;
+        const id = randomUUID();
+        let values = [id, numero, mensagem, tipo, 'pendente'];
 
-router.post("/post", (req, res) => {
-    return res.status(200).json({ message: "Ok" })
+        let sql = 'INSERT INTO notificacoes (id_notificacao, numero_cliente, mensagem, tipo, status) VALUES (?, ?, ?, ?, ?)';
+        const [rows] = await db.execute(sql, values)
+        return res.status(200).json({ message: "Notificação salva com sucesso" })
+
+    } catch (error) {
+        console.error("Erro ao salvar notificação:", error);
+        return res.status(200).json({ message: "Erro ao salvar a notificação"}) //Retorna 200 para não interromper fluxo do N8N
+    }
 })
 
 module.exports = router;
