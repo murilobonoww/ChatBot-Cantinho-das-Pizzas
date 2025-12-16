ee55d54368a94ca1035b13b31d0242aaa8c08d18
murilobonoww <murilobonow07@gmail.com>
2025-12-16 13:03:32 -0300

    feat: add get and put routes to notification

--- back/notificationRoutes.js
+++ back/notificationRoutes.js
@@ -22,13 +22,28 @@ router.post("/post", async (req, res) => {
     }
 })
 
-// router.get("/pendentes", async (req, res) => {
-//     try {
-//         const sql = 'SELECT * FROM notificacoes where status = ?';
-//         const [rows] = 
-//     } catch (error) {
-        
-//     }
-// })
+router.get("/pendentes", async (req, res) => {
+    try {
+        const sql = 'SELECT * FROM notificacoes WHERE status = ?';
+        const [rows] = await db.query(sql, ['pendente']);
+        return res.status(200).json({ message: "Notificações listadas com sucesso" })
+    } catch (error) {
+        console.error("Erro ao salvar notificação:", error);
+        return res.status(200).json({ message: "Erro ao listar notificações"}) //Retorna 200 para não interromper fluxo do N8N
+    }
+})
+
+router.put("/atualizar", async (req, res) => {
+    try {
+        const {id, status} = req.body;
+        const values = [status, id]
+        const sql = 'UPDATE notificacoes SET status = ? where id_notificacao = ?';
+        const [rows] = await db.query(sql, values);
+        return res.status(200).json({ message: "Notificação atualizada com sucesso" })
+    } catch (error) {
+        console.error("Erro ao atualizar notificação:", error);
+        return res.id_(200).json({ message: "Erro ao atualizar notificações"}) //Retorna 200 para não interromper fluxo do N8N
+    }
+})
 
 module.exports = router;
