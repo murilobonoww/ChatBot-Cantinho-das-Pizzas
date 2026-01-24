5a71a08688d145e6f177d4bfc055beb7f101079c
murilobonoww <murilobonow07@gmail.com>
2026-01-23 21:45:55 -0300

    improve UX/UI

--- frontend/src/Pages/Relatorios.jsx
+++ frontend/src/Pages/Relatorios.jsx
@@ -7,6 +7,8 @@ import bell_sound from "/assets/bell.mp3"
 import { Toaster } from "react-hot-toast";
 import { toast } from "react-toastify";
 
+import { PiggyBank, ShoppingBasket, Lock, Award, CreditCard } from 'lucide-react';
+
 export default function Relatorios() {
   const [relatorio, setRelatorio] = useState({});
   const [inicio, setInicio] = useState("");
@@ -16,6 +18,7 @@ export default function Relatorios() {
   const [filtroSelecionado, setFiltroSelecionado] = useState(null);
   const carregamentoInicial = useRef(true);
   const last_time_data = useRef([]);
+  const [toggle_customized_period_window, setToggle_customized_period_window] = useState(false);
 
   const senhaInputRef = useRef(null);
 
@@ -71,7 +74,7 @@ export default function Relatorios() {
     { name: "Crédito", value: relatorio.pagamentos?.crédito || 0 },
   ];
 
-  const COLORS = ["#009247", "#FF7043", "#8303d2"];
+  const COLORS = ["#2dd69c", "#34b2fe", "#895ce1"];
 
   const buscarRelatorio = (start, end) => {
     const params = new URLSearchParams();
@@ -136,12 +139,11 @@ export default function Relatorios() {
         {!autorizado && (
           <div>
             <div className="header-relatorios-input">
-              <h1 id="title_relatorios1">Faturamento</h1>
+              <Lock size={150} strokeWidth={3} id="lock_icon_dashboard" />
             </div>
             <div className="centralizar_gerencia">
-
+              <h1 id="title_locked_dashboard">Digite a senha da gerência</h1>
               <div className="senha-gerencia">
-                <p>Digite a senha da gerência:</p>
                 <div className="input_senha_gerencia">
                   <input
                     type="password"
@@ -151,7 +153,7 @@ export default function Relatorios() {
                     placeholder="Senha"
                     ref={senhaInputRef}
                   />
-                  <button onClick={() => buscarRelatorio()}>Acessar Relatórios</button>
+                  <button onClick={() => buscarRelatorio()}>Acessar dashboard</button>
                 </div>
               </div>
             </div>
@@ -161,42 +163,67 @@ export default function Relatorios() {
         {autorizado && (
           <div className="relatorios_panel">
             <div className="header-relatorios">
-              <h1 id="title_relatorios1">Faturamento</h1>
+              <h1 id="title_relatorios1">Dashboard</h1>
             </div>
             <div className="relatorios-container">
 
               <div className="coluna-esquerda">
-                <div class="filtros-data">
-                  <label>
-                    Início
-                    <input type="date" onChange={(e) => setInicio(e.target.value)} class="inputs-relatorios" />
-                  </label>
-
-                  <label>
-                    Fim
-                    <input type="date" max={new Date().toISOString().split("T")[0]} onChange={(e) => setFim(e.target.value)} class="inputs-relatorios" />
-                  </label>
-
-                  <button id="buscar_btn" onClick={() => buscarRelatorio()}>Buscar</button>
-                </div>
-
                 <div className="filtros-rapidos">
-                  <p>Filtros rápidos:</p>
                   <button className={filtroSelecionado === 0 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(0)}>Hoje</button>
-                  <button className={filtroSelecionado === 7 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(7)}>Últimos 7 dias</button>
+                  <button className={filtroSelecionado === 7 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(7)}>Esta semana</button>
                   <button className={filtroSelecionado === 15 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(15)}>Últimos 15 dias</button>
-                  <button className={filtroSelecionado === 30 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(30)}>Últimos 30 dias</button>
+                  <button className={filtroSelecionado === 30 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(30)}>Este mês</button>
                   <button className={filtroSelecionado === 90 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(90)}>Últimos 3 meses</button>
-                  <button className={filtroSelecionado === 365 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(365)}>Últimos 12 meses</button>
-                  <button className={filtroSelecionado === 999 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(999)}>Todos</button>
+                  <button className={filtroSelecionado === 365 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(365)}>Este ano</button>
+                  <button className={filtroSelecionado === 999 ? "filtro-ativo" : ""} onClick={() => aplicarFiltroRapido(999)}>Tudo</button>
+                  <button onClick={() => setToggle_customized_period_window(!toggle_customized_period_window)} style={{ background: "#ff7043", color: "white", padding: "10px" }}>Customizar período</button>
                 </div>
+
+{/* <div className="filter"></div> */}
+                  <div class="filtros-data" style={{ opacity: toggle_customized_period_window === true ? "100" : "0", pointerEvents: toggle_customized_period_window === true ? "auto" : "none", position: "fixed", zIndex: "3000", top: "50%", left: "50%", transform: "translate(-50%, -80%)", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <h1 style={{ marginBottom: "10px" }}>Customizar período</h1>
+                    <div style={{ display: "flex", gap: "20px", marginTop: "10px", alignItems: "center",  }}>
+                    <label>
+                      De
+                      <input type="date" onChange={(e) => setInicio(e.target.value)} class="inputs-relatorios" />
+                    </label>
+
+                    <label>
+                      Até
+                      <input type="date" max={new Date().toISOString().split("T")[0]} onChange={(e) => setFim(e.target.value)} class="inputs-relatorios" />
+                    </label>
+                    <button id="buscar_btn" onClick={() => buscarRelatorio()}>Buscar</button>
+                    </div>
+                  </div>
 
                 <div className="resumos">
+                  <div className="card">
+                    <PiggyBank size={45} style={{ padding: "5", backgroundColor: "#895ce1", color: "white", borderRadius: "50%", transform: "scaleX(-1)", strokeWidth: "1", marginRight: "10" }} />
+                    <div style={{ display: "flex", flexDirection: "column", textAlign: "start" }}>
+                      <span style={{ color: "gray" }}>Faturamento:</span> <strong>   <span style={{ fontSize: "20px", color: "#525252" }} >R$ {relatorio.total_vendas?.toFixed(2).replace(".", ",") || "0,00"}</span> </strong>
+                    </div>
+                  </div>
 
-                  <div className="card">Total em vendas: <strong>R$ {relatorio.total_vendas?.toFixed(2).replace(".", ",") || "0,00"}</strong></div>
-                  <div className="card">Pedidos no período: <strong>{relatorio.total_pedidos || 0}</strong></div>
-                  <div className="card">Ticket médio: <strong>R$ {relatorio.ticket_medio?.toFixed(2).replace(".", ",") || "0,00"}</strong></div>
-                  <div className="card">Mais vendido: <strong>{relatorio.mais_vendido || "-"}</strong></div>
+                  <div className="card">
+                    <ShoppingBasket size={45} style={{ padding: "5", backgroundColor: "#2dd69c", color: "white", borderRadius: "50%", transform: "scaleX(-1)", strokeWidth: "1", marginRight: "10" }} />
+                    <div style={{ display: "flex", flexDirection: "column", textAlign: "start" }}>
+                      <span style={{ color: "gray" }}>Qtd pedidos:</span> <strong>   <span style={{ fontSize: "20px", color: "#525252" }} >{relatorio.total_pedidos || "0"}</span> </strong>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <CreditCard size={45} style={{ padding: "5", backgroundColor: "#34b2fe", color: "white", borderRadius: "50%", transform: "scaleX(-1)", strokeWidth: "1", marginRight: "10" }} />
+                    <div style={{ display: "flex", flexDirection: "column", textAlign: "start" }}>
+                      <span style={{ color: "gray" }}>Ticket médio:</span> <strong>   <span style={{ fontSize: "20px", color: "#525252" }} >R$ {relatorio.ticket_medio?.toFixed(2).replace(".", ",") || "0,00"}</span> </strong>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <Award size={45} style={{ padding: "5", backgroundColor: "#ffaa7b", color: "white", borderRadius: "50%", transform: "scaleX(-1)", strokeWidth: "1", marginRight: "10" }} />
+                    <div style={{ display: "flex", flexDirection: "column", textAlign: "start" }}>
+                      <span style={{ color: "gray" }}>Mais vendido:</span> <strong>   <span style={{ fontSize: "20px", color: "#525252" }} >{relatorio.mais_vendido || "-"}</span> </strong>
+                    </div>
+                  </div>
                 </div>
 
                 {relatorio.total_vendas > 0 ? (
@@ -223,10 +250,9 @@ export default function Relatorios() {
                 ) : (
                   <p className="txt_relatorios_not_found">Não encontramos vendas neste período</p>
                 )}
-
               </div>
 
-              <div className="coluna-direita">
+              {relatorio.total_vendas > 0 && (<div className="coluna-direita">
                 <h2>Últimos pedidos</h2>
                 <div className="tabela-container">
                   <table className="tabela-pedidos">
@@ -251,7 +277,8 @@ export default function Relatorios() {
                     </tbody>
                   </table>
                 </div>
-              </div>
+              </div>)}
+
             </div>
           </div>
         )}
