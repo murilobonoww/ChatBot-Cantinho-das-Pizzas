7db5dba95dfdda0d7b5a0423ef3e9e83a4932599
Murilo <murilobonow07@gmail.com>
2026-02-14 17:22:28 -0300

    fix(front): corrige rota fantasma

--- frontend/src/app/AppRoutes.jsx
+++ frontend/src/app/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import PrivateRoute from "@/Components/PrivateRoute";
 import Home from "@/Pages/Home";
 import Pedidos from "@/Pages/Pedidos";
@@ -10,9 +10,9 @@ import Layout from "../Components/Layout";
 export default function AppRoutes({ pedidos, setPedidos }) {
     return(
         <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route element={ <Layout /> }>
-        {/* <Route path="/" element={ <PrivateRoute> <Home /> </PrivateRoute> } /> */}
         <Route path="/pedidos" element={ <PrivateRoute> <Pedidos pedidos={pedidos} setPedidos={setPedidos} /> </PrivateRoute> } />
         <Route path="/relatorios" element={ <PrivateRoute> <Relatorios /> </PrivateRoute> } />
         <Route path="/cardapio" element={ <PrivateRoute> <Cardapio /> </PrivateRoute> } />
