109f4d09ab2a0830397e971b8df789fbac323981
murilobonoww <murilobonow07@gmail.com>
2026-06-13 16:33:28 -0300

    adicionar token via req authorization para facilitar testes

--- back/src/middlewares/autenticarUser.js
+++ back/src/middlewares/autenticarUser.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET_KEY } = process.env;
 
 module.exports = function autenticarUser (req, res, next) {
-  const token = req.cookies?.token;
+  const token = req.cookies?.token ?? req.headers["authorization"]?.split(" ")[1];
 
   if (!token) return res.status(401).json({ error: 'Token not provided' })
 
